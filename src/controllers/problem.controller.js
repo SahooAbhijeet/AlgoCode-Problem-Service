c361b4b1b2680700420a8ddbf4dce461add580d7
ABHIJEET <abhijeetsahoomsd@gmail.com>
2024-06-04 19:13:03 +0530

    FEAT: Added routing structure for the versioned api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,35 @@
+function pingProblemController(req, res) {
+    return res.json({
+        message: 'Ping Controller is UP'
+    })
+}
+
+function addProblem(req,res) {
+
+}
+
+function getProblem(req,res) {
+    
+}
+
+function getProblems(req,res) {
+    
+}
+
+function deleteProblem(req,res) {
+    
+}
+
+function updateProblem(req,res) {
+    
+}
+
+module.exports = {
+    pingProblemController,
+    addProblem,
+    getProblem,
+    getProblems,
+    deleteProblem,
+    updateProblem
+
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const {ServerConfig} = require('./config');
-
+const apiRouter  = require('./routes')
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.text());
+
+app.use('/api', apiRouter);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server started at  ${ServerConfig.PORT}`);
